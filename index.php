760c7e99011874a5fadcc5b632670b5db3b10f11
Ye Ding <guiewy@gmail.com>
2016-05-24 13:40:32 +0800

    v0.5.24

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7">
+<![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8">
+<![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>NiCPU3</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all"/>
+    <!--[if gte IE 9]>
+    <link rel="stylesheet" type="text/css" href="css/ie9.css"/>
+    <![endif]-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700%7CRaleway:700' rel='stylesheet' type='text/css'>
+    <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body>
+<div class="loader">
+    <div class="spinner">
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+    </div>
+</div>
+<div class="nav-container">
+    <nav class="top-bar overlay-bar offscreen-menu">
+        <div class="container">
+            <div class="row nav-menu clearfix">
+                <div class="col-sm-2 col-xs-6">
+                    <a href="index.php">
+                        <img class="logo logo-light" alt="Logo" src="img/logo-light.png">
+                        <img class="logo logo-dark" alt="Logo" src="img/logo-dark.png">
+                    </a>
+                </div>
+                <div class="col-sm-2 col-xs-6 text-right pull-right">
+                    <div class="offscreen-toggle">
+                        <i class="icon icon_menu text-white"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <hr class="no-margin">
+                </div>
+            </div>
+        </div>
+        <div class="offscreen-container">
+            <img alt="logo" class="logo" src="img/logo-light.png">
+            <ul class="menu">
+                <li><a href="https://nicpu3.cse.ust.hk" class="inner-link" target="_blank">GitLab</a></li>
+                <li><a href="http://nicpu3.cse.ust.hk:8080" class="inner-link" target="_blank">Wetty</a></li>
+                <li><a href="avatar" class="inner-link" target="_blank">Avatar</a></li>
+                <li><a href="joker" class="inner-link" target="_blank">SmartCube</a></li>
+                <li><a href="lottery" class="inner-link" target="_blank">Lottery</a></li>
+                <li><a href="franz" class="inner-link" target="_blank">DMP</a></li>
+                <li><a href="hec" class="inner-link" target="_blank">Hec Casino</a></li>
+            </ul>
+            <ul class="social-icons">
+                <li>
+                    <a href="#">
+                        <i class="icon social_twitter"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="icon social_facebook"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="icon social_instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<div class="main-container"><a id="home" class="in-page-link"></a>
+    <header class="fullscreen-element no-pad centered-text">
+        <div class="background-image-holder parallax-background overlay">
+            <img class="background-image" alt="Background Image" src="img/hero8.jpg">
+        </div>
+        <div class="container align-vertical">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <span class="text-white alt-font">Valency Presents</span>
+                    <h1 class="text-white">The NiCPU Server #3</h1>
+                    <p class="lead text-white">Click the Top Right Button to Show Projects</p>
+                    <a target="_blank" href="http://www.cse.ust.hk/~valency" class="btn btn-primary btn-white">Copyright</a>
+                </div>
+            </div>
+        </div>
+    </header>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.plugin.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/smooth-scroll.min.js"></script>
+<script src="js/skrollr.min.js"></script>
+<script src="js/spectragram.min.js"></script>
+<script src="js/scrollReveal.min.js"></script>
+<script src="js/isotope.min.js"></script>
+<script src="js/twitterFetcher_v10_min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
